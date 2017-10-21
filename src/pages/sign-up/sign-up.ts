cd1c75184cc924e278a279f9a3164e3405ac9602
azxy <sgopinath.sms@gmail.com>
2017-10-21 09:57:04 +0530

    Corrections

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,15 +1,7 @@
-import { AppConstants } from '../../app/app.constants';
 import { AppConfig } from '../../app/app.config';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignUpPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -17,8 +9,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'sign-up.html',
 })
 export class SignUpPage {
-signUpForm: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder, private config: AppConfig, private constants: AppConstants) {
+  signUpForm: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder, private config: AppConfig) {
     this.createSignUpForm();
   }
 
@@ -26,12 +18,12 @@ signUpForm: any;
     console.log('ionViewDidLoad SignUpPage');
   }
 
-  createSignUpForm(){
-    this.signUpForm = this.fb.group({ // <-- the parent FormGroup
-      name: ['', Validators.required, Validators.minLength(5) ],
-      email: ['', Validators.required, Validators.pattern(this.config.emailRegex) ],
-      password: ['', Validators.required, Validators.minLength(5) ],
-      passwordRepeat: ['', Validators.required, Validators.minLength(5) ],
+  createSignUpForm() {
+    this.signUpForm = this.fb.group({
+      name: ['', Validators.required, Validators.minLength(5)],
+      email: ['', Validators.required, Validators.pattern(this.config.emailRegex)],
+      password: ['', Validators.required, Validators.minLength(5)],
+      passwordRepeat: ['', Validators.required, Validators.minLength(5)],
       address: this.fb.group({
         no: '',
         street: '',
